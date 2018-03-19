5910dbb3050d7f7a2535ca9fc3ba076f673bb3d0
Matthew Daly <matthewbdaly@gmail.com>
2018-03-19 21:40:36 +0000

    Added argument to processor

--- src/Commands/Processor.php
+++ src/Commands/Processor.php
@@ -5,6 +5,8 @@ namespace Matthewbdaly\LetterClassifier\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use League\Pipeline\Pipeline;
 
 class Processor extends Command
 {
@@ -12,12 +14,13 @@ class Processor extends Command
     {
         $this->setName('process')
             ->setDescription('Processes a file')
-            ->setHelp('This command processes a file');
+            ->setHelp('This command processes a file')
+            ->addArgument('file', InputArgument::REQUIRED, 'File to process');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+        $file = $input->getArgument('file');
     }
 }
 
