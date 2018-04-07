67244a5fdcc557cc4582b5e42879a9692c324e75
Matthew Daly <matthewbdaly@gmail.com>
2018-04-07 22:31:37 +0100

    Some progress with the classifier

--- src/Stages/Classify.php
+++ src/Stages/Classify.php
@@ -2,9 +2,27 @@
 
 namespace Matthewbdaly\LetterClassifier\Stages;
 
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\ModelManager;
+
 class Classify
 {
+    protected $classifier;
+
+    protected $manager;
+
+    public function __construct()
+    {
+        $this->manager = new ModelManager;
+        if (file_exists('model.txt')) {
+            $this->classifier = $this->manager->restoreFromFile('model.txt');
+        } else {
+            $this->classifier = new KNearestNeighbors;
+        }
+    }
+
     public function __invoke(string $text)
     {
+        $this->classifier->predict([$text]);
     }
 }
