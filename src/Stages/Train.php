2f38290161a7b97fbc37cfc5d785a9d9ef7383b1
Matthew Daly <matthewbdaly@gmail.com>
2018-04-07 22:28:33 +0100

    Can now train on documents

--- src/Stages/Train.php
+++ src/Stages/Train.php
@@ -2,17 +2,31 @@
 
 namespace Matthewbdaly\LetterClassifier\Stages;
 
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\ModelManager;
+
 class Train
 {
     protected $category;
 
+    protected $classifier;
+
+    protected $manager;
+
     public function __construct(string $category)
     {
         $this->category = $category;
+        $this->manager = new ModelManager;
+        if (file_exists('model.txt')) {
+            $this->classifier = $this->manager->restoreFromFile('model.txt');
+        } else {
+            $this->classifier = new KNearestNeighbors;
+        }
     }
 
     public function __invoke(string $text)
     {
-        eval(\Psy\Sh());
+        $this->classifier->train([$text], [$this->category]);
+        $this->manager->saveToFile($this->classifier, 'model.txt');
     }
 }
